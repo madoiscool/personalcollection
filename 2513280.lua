6dfadbba0fd839d433bbed2f9d3d73da34dad3e1
madoiscool <110845835+madoiscool@users.noreply.github.com>
2025-11-30 19:41:18 +1100

    luas

---
+++ 2513280.lua
@@ -0,0 +1,9 @@
+addappid(2513280)
+addappid(2513281,0,"e5b9cddfe91ed04af8c6d17537301797ab16a5041a7edca1d45ae281208a740f")
+--setManifestid(2513281,"1703635842257284304")
+-- Non-Encrypted DLC ID's (Added by LuaTools @https://discord.gg/NGYPY524 )
+addappid(2909770) -- SONIC X SHADOW GENERATIONS: Extra Content Pack
+addappid(2983600) -- SONIC X SHADOW GENERATIONS: Sonic the Hedgehog 3 Movie Pack
+addappid(2909780) -- SONIC X SHADOW GENERATIONS: Sonic Adventure Legacy Skin
+addappid(2999160) -- SONIC X SHADOW GENERATIONS: Art Book with Mini Soundtrack
+addappid(2909790) -- SONIC X SHADOW GENERATIONS: Sonic Jam Legacy Skin
